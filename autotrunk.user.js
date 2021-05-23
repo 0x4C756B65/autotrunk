15c449c39b62de70c4faeb32fda70558836f993f
Luke <82839436+0x4C756B65@users.noreply.github.com>
2021-05-23 16:34:46 +0100

    Add autotrunk.user.js

---
+++ autotrunk.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         CryptoTrunks
+// @version      0.1
+// @description  Automate CryptoTrunks generation to find specified property
+// @author       Luke
+// @match        https://cryptotrunks.co/generate-in-progress
+// ==/UserScript==
+
+var PROPERTY1 = "Sapling";
+var PROPERTY2 = "Evening";
+var PROPERTY3 = "Grass";
+
+
+setTimeout(function() {
+
+var content = document.body.textContent || document.body.innerText;
+var hasProperty = content.indexOf(PROPERTY1)!==-1;
+var hasProperty2 = content.indexOf(PROPERTY2)!==-1;
+var hasProperty3 = content.indexOf(PROPERTY3)!==-1;
+
+if(hasProperty && hasProperty2 && hasProperty3){
+    alert("CryptoTrunk Property Found");
+}else{
+    console.log("Property Not Found");
+    document.location.reload();
+}
+
+}, 15000);
